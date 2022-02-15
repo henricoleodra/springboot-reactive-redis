2fa57dd3bf0a0c57759a7449318ef454ef51d525
Henrico Leodra <henricoleodra@gmail.com>
2022-02-15 15:06:42 +0700

    [INIT] Spring Webflux + Redis Reactive

---
+++ src/main/java/com/henricoleodra/springreactiveredis/SpringreactiveredisApplication.java
@@ -0,0 +1,13 @@
+package com.henricoleodra.springreactiveredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringreactiveredisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringreactiveredisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henricoleodra/springreactiveredis/SpringreactiveredisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henricoleodra.springreactiveredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringreactiveredisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
